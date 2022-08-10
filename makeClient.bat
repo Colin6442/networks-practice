a1d8d4189ea93ca2ad7b568ca06db6f8d8dfe0b0
Colin S <colin6442@gmail.com>
2022-08-09 21:48:46 -0700

    added complie scripts

---
+++ makeClient.bat
@@ -0,0 +1,5 @@
+@echo OFF
+
+c:\windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:exe /out:client.exe client.cs Form1.cs Form1.Designer.cs
+
+pause
---
+++ makeServer.bat
@@ -0,0 +1,5 @@
+@echo OFF
+
+c:\windows\Microsoft.NET\Framework\v4.0.30319\csc.exe /t:exe /out:server.exe server.cs
+
+pause
